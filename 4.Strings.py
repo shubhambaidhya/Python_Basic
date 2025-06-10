4b1c28e6e64b1f746964658ff677435360462401
shubhambaidhya <shubhambaidhya7@gmail.com>
2025-06-10 20:01:45 +0545

    Strings

---
+++ 4.Strings.py
@@ -0,0 +1,41 @@
+# Strings => strings in python are surrounded by either the single quotation or double quotation marks.
+# Eg: 'Hello' / "Hello"
+
+#Strings as an Array
+#* Python doesn't have a character datatype so, a single character is simply a string with length 1
+
+#------------------Example 1-------------------------
+
+a ='Hello World!'
+print(a[1]) #the output is e cause the first index starts with 0
+
+
+
+#Check String => we can use keyword 'in' to check if a certain phrase or character is present in a string or not 
+
+#-------------------Example 2--------------------------
+
+txt = 'The best things in life are free!'
+if "free" in txt:                       # used if statement as well
+    print("Yes, 'free' is present.")
+
+
+#Slicing Strings => You can return a range of characters by using the slice syntax
+
+#-------------------Example 3-------------------------
+
+b="Hello World!"
+print(b[2:5])
+
+
+#Slice from the start
+#------------------Example 4-------------------------
+
+b="Hello World!"
+print(b[:5])    #Get the characters from the start to position 5 (not included)
+
+
+#-----------------Example 5---------------------------
+
+b="Hello World!"
+print(b[-5:-2])
